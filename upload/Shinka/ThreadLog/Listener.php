1f5f51aeb0bd5ff40cf8c9f48da6d1d3a17bb9b8
kalynrobinson <kalynrobinson@gmail.com>
2017-10-31 15:49:44 -0400

    Add listener to extend Thread entity to manage relationship with ThreadLog.

---
+++ upload/Shinka/ThreadLog/Listener.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Extends the XF\Entity\Thread structure to manage relationship with ThreadLog.
+ */
+namespace Shinka\ThreadLog;
+
+use XF\Mvc\Entity\Entity;
+
+class Listener
+{
+    public static function threadEntityStructure(\XF\Mvc\Entity\Manager $em, \XF\Mvc\Entity\Structure &$structure)
+    {
+        $structure->relations['ThreadLog'] = [
+            'entity' => 'Shinka\ThreadLog:ThreadLog',
+            'type' => Entity::TO_ONE,
+            'conditions' => 'thread_id',
+            'primary' => true
+        ];
+    }
+}
