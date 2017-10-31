1d225b0efa64f112be7e277fe4524d769a6fb442
kalynrobinson <kalynrobinson@gmail.com>
2017-10-31 16:50:40 -0400

    Add view for thread log action.

--- upload/Shinka/ThreadLog/XF/Pub/Controller/Member.php
+++ upload/Shinka/ThreadLog/XF/Pub/Controller/Member.php
@@ -2,9 +2,21 @@
 
 namespace Shinka\ThreadLog\XF\Pub\Controller;
 
+use XF\Mvc\ParameterBag;
+
 class Member extends XFCP_Member
 {
-    public function actionThreadLog() {
-        return $this->message('Hello world!');
+    public function actionThreadLog(ParameterBag $params)
+    {
+        $user = $this->assertViewableUser($params->user_id);
+
+        $page = $this->filterPage($params->page);
+        $perPage = $this->options()->threadsPerPage;
+		$threadLogRepo = $this->repository('Shinka\ThreadLog:ThreadLog');
+
+        $viewParams = [
+            'user' => $user
+        ];
+        return $this->view('Shinka\ThreadLog:View', 'shinka_threadlog_member_threadlog', $viewParams);
     }
 }
